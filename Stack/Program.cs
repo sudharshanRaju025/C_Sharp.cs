20b7d1bc31573180c5b3f30a55c8ccc9691d166d
sudharshanraju025 <bsudharshan404@gmail.com>
2025-05-02 16:54:17 +0530

    practice

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -10,7 +10,19 @@ namespace Collections.cs
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(134);
+            stack.Push(1321);
+            stack.Push(198);
+            stack.Push(364);
+            stack.Push(100);
+
+            int check=stack.Peek();
+            Console.WriteLine(check);
+            int remove = stack.Pop();
+            Console.WriteLine(remove);
+
 
         }
     }
