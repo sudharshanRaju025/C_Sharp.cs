4887f0ce72ab921b0a8f510ace47c16e3ff358fc
sudharshanraju025 <bsudharshan404@gmail.com>
2025-05-17 18:43:15 +0530

    sorting

---
+++ DSA_Practice/HeapSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Practice
+{
+    class Heap
+    {
+
+    }
+     class HeapSort
+    {
+        public void HeapSortMethod(int[] A,int n)
+        {
+            Heap heap1 = new Heap();
+            for(int i=0; i<A.Length; i++)
+            {
+                heap1.insert(A[i]);
+            }
+
+        }
+    }
+}
